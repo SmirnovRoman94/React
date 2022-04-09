d0db7f0db38a96c8509977d4c41be2e9b3f7b21f
SmirnovRoman94 <smirnow3217831@gmail.com>
2022-04-09 20:38:41 +0300

    homework 4
    homework 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.scss';
-import { LoremIpsum} from 'react-lorem-ipsum';
-import Message from './Message';
+import Router from './pages/Router'
 
-const messageItem = "Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis libero feugiat semper aliquet eget phasellus. Inceptos fringilla ultrices eu libero cubilia tortor amet. Phasellus habitasse urna maecenas eget suscipit; leo rhoncus. Ultrices habitasse ultricies bibendum diam turpis nostra."
 
-function App() {
+
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <h1>My first App</h1>
+          <h1>Render props</h1>
+          <Router/>
         </div>
       </header>
-      <div class="container">
-        <LoremIpsum p={3}/>
-        <div class="mes">
-          <Message mes={messageItem}/>
-        </div>
-      </div>
-    </div>
+  </div>
+
   );
 }
 
